51b34fc1bfff1af74aad56a5e61f7fef2d8c0760
Maximiliano Nadalin <maximilianonadalin18@gmail.com>
2023-11-29 22:02:20 -0300

    add layouts

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require ("express");
 const app = express();
 
-app.use(express.static("public"));/*para decir que cosas son publicas*/
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static("public"));/*para decir que cosas son publicas*/
 
 app.set("view engine" , "ejs");
 app.set("views", "./src/views");/*a donde van a estar las vistas que vamos a utilizar*/
+
+app.use(expressLayouts);
+app.set("layout","layouts/layout");
+
 
 app.use(require('./src/routers/tareasRouter')); 
 
